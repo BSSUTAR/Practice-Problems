cb91ee4499f112107086c698a17d425eaef48dae
BSSUTAR <bss8236@gmail.com>
2016-02-23 20:41:01 +0530

    Problem Statement added

---
+++ career_cup_longest_streak.cpp
@@ -0,0 +1,15 @@
+/*
+ amazon-interview-questions
+
+ Given an array of 0s and 1s, and k, Find the longest continuous streak of 1s after flipping k 0s to 1s.
+
+ E.x array is {1,1,0,0,1,1,1,0,1,1}
+ k = 1 (which means we can flip ‘k’ one 0 to 1)
+
+ Answer: 6 (if we flip 0 at index 7, we get the longest continuous streak of 1s having length 6)
+ Author:BSS
+ Date:23/02/2016 08:38 PM
+ Version:0.0
+*/
+#include<iostream>
+using namespace std;
